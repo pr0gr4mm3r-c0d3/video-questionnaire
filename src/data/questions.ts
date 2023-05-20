c0e1a66e4b6381c4e06df4c6d8f3673af4c07a57
Jonh Elton Serrano <j.elton9669@gmail.com>
2023-05-19 22:41:02 -0400

    [feat]: add questions Data

---
+++ src/data/questions.ts
@@ -0,0 +1,24 @@
+import { IQuestion } from '@/interfaces';
+
+export const questions: IQuestion[] = [
+	{
+		id: 'IdQuestion1',
+		done: false,
+		question: 'Some Question - 1 ?',
+	},
+	{
+		id: 'IdQuestion2',
+		done: false,
+		question: 'Some Question - 2 ?',
+	},
+	{
+		id: 'IdQuestion3',
+		done: false,
+		question: 'Some Question - 3 ?',
+	},
+	{
+		id: 'IdQuestion4',
+		done: false,
+		question: 'Some Question - 4 ?',
+	},
+];
