3233ca4f8baa15c8aa62ffc1b90520b90395f2b3
Jonh Elton Serrano <j.elton9669@gmail.com>
2023-05-23 22:34:42 -0400

    [feat]: Add more data test

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -3,22 +3,40 @@ import { IQuestion } from '@/interfaces';
 export const questions: IQuestion[] = [
 	{
 		id: 'IdQuestion1',
-		done: true,
-		question: 'Some Question - 1 ?',
+		done: false,
+		question: '1 - Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.?',
+		videoRecorder: null,
 	},
 	{
 		id: 'IdQuestion2',
 		done: false,
-		question: 'Some Question - 2 ?',
+		question: '2 - Lorem ipsum dolor sit amet. ?',
+		videoRecorder: null,
 	},
 	{
 		id: 'IdQuestion3',
 		done: false,
-		question: 'Some Question - 3 ?',
+		question: '3 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati deleniti ?',
+		videoRecorder: null,
 	},
 	{
 		id: 'IdQuestion4',
 		done: false,
-		question: 'Some Question - 4 ?',
+		question: '4 - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, ad.?',
+		videoRecorder: null,
+	},
+	{
+		id: 'IdQuestion5',
+		done: false,
+		question:
+			'5 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium quas modi fuga cupiditate quia corporis quae! Neque a sit labore!?',
+		videoRecorder: null,
+	},
+	{
+		id: 'IdQuestion6',
+		done: false,
+		question:
+			'6 - Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis totam reprehenderit rerum itaque eius quos.?',
+		videoRecorder: null,
 	},
 ];
