b7cf9f1fc6b97a885d3bc27bf9d0b4574663bd08
Jonh Elton Serrano <j.elton9669@gmail.com>
2023-05-23 22:40:14 -0400

    [fix]: Use new questions context in layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,24 +1,13 @@
-import { useMemo, useState } from 'react';
 import Container from '@mui/material/Container';
 import { Outlet } from 'react-router-dom';
-import { QuestionsContext } from '@/context';
-import { questions as questionsData } from '@/data';
+import { QuestionsProvider } from '@/context';
 
 export const Layout = () => {
-	const [questions, setQuestions] = useState(questionsData);
-
-	const contextValue = useMemo(() => {
-		return {
-			questions,
-			setQuestions,
-		};
-	}, [questions]);
-
 	return (
 		<Container maxWidth='xl' className='app__container'>
-			<QuestionsContext.Provider value={contextValue}>
+			<QuestionsProvider>
 				<Outlet />
-			</QuestionsContext.Provider>
+			</QuestionsProvider>
 		</Container>
 	);
 };
