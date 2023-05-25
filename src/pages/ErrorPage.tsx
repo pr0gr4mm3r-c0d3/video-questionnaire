21c94fe602b112a7bd35efa308034849011ecea6
Jonh Elton Serrano <j.elton9669@gmail.com>
2023-05-25 01:33:28 -0400

    [style]: Add styles for page 404

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,3 +1,18 @@
+import { BtnComponent } from '@/components';
+import { Container, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
 export const ErrorPage = () => {
-	return <h3>ErrorPage</h3>;
+	const navigate = useNavigate();
+	const handleGoToHome = () => {
+		navigate('/', { replace: true });
+	};
+	return (
+		<Container className='page__404'>
+			<Typography align='center' variant='h3'>
+				Página no encontrada
+			</Typography>
+			<BtnComponent label='Ir a Inicio' onClick={handleGoToHome} />
+		</Container>
+	);
 };
